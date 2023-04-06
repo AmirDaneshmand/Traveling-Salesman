83799a75a0d9f3e92f11ef7eaf945594dd1326b9
AmirReza <d1382.1392@gmail.com>
2023-04-06 07:16:17 +0430

    made main plane

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 public class Game extends JFrame implements ActionListener{
     GameBoard board;
+
+    private Random random;
     private Game me;
     private int cellSize;
     private int topMargin,leftMargin;
     public Image castleIcn = new ImageIcon("castle.png").getImage();
+    public Image marketIcn = new ImageIcon("market.png").getImage();
+
+    public Image treasureIcn = new ImageIcon("treasure.png").getImage();
 
 
+    public int[][] treasureLoc = new int[8][2];
+    int usedTreasureNumber;
+    /*
+    1 = angoshtar
+    2 = shamshir
+    3 = livan
+    4 = jamm
+    5 = kaman
+    6 = separ
+    7 = kid
+    8 = tomar
+     */
+    public int[][] MarketLocVar = new int[5][2];
+
+    public  int[][] UsedNumberInt;
+    int UsedNumberIntArrey = 0;
     public static JButton backBtnGame;
     random_number diceRand = new random_number();
 
@@ -25,10 +47,9 @@ public class Game extends JFrame implements ActionListener{
         cellSize = 60;
         topMargin = 80;
         leftMargin = 500;
-
-       initTable();
-       initPanle();
-
+        initTable();
+        initPanle();
+        initObjectLoc();
 
 
 
@@ -56,6 +77,351 @@ public class Game extends JFrame implements ActionListener{
 
     }
 
+    private void initObjectLoc() {
+        random = new Random();
+        UsedNumberInt = new int[20][2];
+
+
+
+
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 2; j++) {
+                UsedNumberInt[i][j] = -1;
+            }
+        }
+
+        UsedNumberInt[UsedNumberIntArrey] = new int[]{6, 6};
+        UsedNumberIntArrey++;
+
+        while (true) {
+            boolean OutOfLoop = true;
+
+            MarketLocVar[0][0] = random.nextInt(0, 6);
+            MarketLocVar[0][1] = random.nextInt(0, 6);
+            for (int i = 0; i < 20; i++) {
+                if (MarketLocVar[0][0] != UsedNumberInt[i][0]
+                || MarketLocVar[0][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = MarketLocVar[0];
+                UsedNumberIntArrey++;
+                break;
+            }
+
+        }
+
+        while (true) {
+            boolean OutOfLoop = true;
+
+            MarketLocVar[1][0] = random.nextInt(0, 6);
+            MarketLocVar[1][1] = random.nextInt(0, 6);
+            for (int i = 0; i < 20; i++) {
+                if (MarketLocVar[1][0] != UsedNumberInt[i][0]
+                        || MarketLocVar[1][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = MarketLocVar[1];
+                UsedNumberIntArrey++;
+
+                break;
+            }
+
+        }
+
+        while (true) {
+            boolean OutOfLoop = true;
+
+            MarketLocVar[2][0] = random.nextInt(7, 11);
+            MarketLocVar[2][1] = random.nextInt(0, 6);
+            for (int i = 0; i < 20; i++) {
+                if (MarketLocVar[2][0] != UsedNumberInt[i][0]
+                        || MarketLocVar[2][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = MarketLocVar[2];
+                UsedNumberIntArrey++;
+
+                break;
+            }
+
+        }
+
+        while (true) {
+            boolean OutOfLoop = true;
+
+            MarketLocVar[3][0] = random.nextInt(0, 6);
+            MarketLocVar[3][1] = random.nextInt(7, 11);
+            for (int i = 0; i < 20; i++) {
+                if (MarketLocVar[3][0] != UsedNumberInt[i][0]
+                        || MarketLocVar[3][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = MarketLocVar[3];
+                UsedNumberIntArrey++;
+
+                break;
+            }
+
+        }
+
+        while (true) {
+            boolean OutOfLoop = true;
+
+            MarketLocVar[4][0] = random.nextInt(7, 11);
+            MarketLocVar[4][1] = random.nextInt(7, 11);
+            for (int i = 0; i < 20; i++) {
+                if (MarketLocVar[4][0] != UsedNumberInt[i][0]
+                        || MarketLocVar[4][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = MarketLocVar[4];
+                UsedNumberIntArrey++;
+
+                break;
+            }
+
+        }
+        /*
+        --------------------------------------------------------------
+        */
+
+        for (int i = 0; i < 8; i++) {
+            treasureLoc[i][0] = -1;
+            treasureLoc[i][1] = -1;
+        }
+
+        int[] usedTreasure = new int[8];
+        usedTreasureNumber = 0;
+
+        for (int i = 0; i < 8; i++) {
+            usedTreasure[i] = -1;
+        }
+        //random select for treasure
+
+        for (int j = 0; j <8; j++) {
+
+            while (true) {
+                boolean outOfLoopTreasure = true;
+                int SelectTreasure = random.nextInt(0, 8);
+                for (int i = 0; i < 8; i++) {
+                    if (SelectTreasure != usedTreasure[i]) {
+                        continue;
+                    } else {
+                        outOfLoopTreasure = false;
+                        break;
+                    }
+                }
+                    System.out.println(outOfLoopTreasure);
+                if (outOfLoopTreasure) {
+                    usedTreasure[usedTreasureNumber] = SelectTreasure;
+                    usedTreasureNumber++;
+                    break;
+                }
+
+            }
+
+        }
+
+
+
+        //random insert for random treasure
+      while (true){
+            boolean OutOfLoop = true;
+            treasureLoc[usedTreasure[0]][0] = random.nextInt(0,6);
+            treasureLoc[usedTreasure[0]][1] = random.nextInt(0,6);
+            for (int i = 0; i < 20; i++) {
+                if ( treasureLoc[usedTreasure[0]][0] != UsedNumberInt[i][0]
+                || treasureLoc[usedTreasure[0]][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = treasureLoc[usedTreasure[0]];
+                UsedNumberIntArrey++;
+
+                break;
+            }
+    }
+
+      while (true){
+            boolean OutOfLoop = true;
+            treasureLoc[usedTreasure[1]][0] = random.nextInt(0,6);
+            treasureLoc[usedTreasure[1]][1] = random.nextInt(0,6);
+            for (int i = 0; i < 20; i++) {
+                if ( treasureLoc[usedTreasure[1]][0] != UsedNumberInt[i][0]
+                        || treasureLoc[usedTreasure[1]][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = treasureLoc[usedTreasure[1]];
+                UsedNumberIntArrey++;
+                break;
+            }
+        }
+
+      while (true){
+            boolean OutOfLoop = true;
+            treasureLoc[usedTreasure[2]][0] = random.nextInt(7,11);
+            treasureLoc[usedTreasure[2]][1] = random.nextInt(0,6);
+            for (int i = 0; i < 20; i++) {
+                if ( treasureLoc[usedTreasure[2]][0] != UsedNumberInt[i][0]
+                        || treasureLoc[usedTreasure[2]][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = treasureLoc[usedTreasure[2]];
+                UsedNumberIntArrey++;
+                break;
+            }
+        }
+
+      while (true){
+            boolean OutOfLoop = true;
+            treasureLoc[usedTreasure[3]][0] = random.nextInt(7,11);
+            treasureLoc[usedTreasure[3]][1] = random.nextInt(2,6);
+            for (int i = 0; i < 20; i++) {
+                if ( treasureLoc[usedTreasure[3]][0] != UsedNumberInt[i][0]
+                        || treasureLoc[usedTreasure[3]][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = treasureLoc[usedTreasure[3]];
+                UsedNumberIntArrey++;
+                break;
+            }
+        }
+
+      while (true){
+            boolean OutOfLoop = true;
+            treasureLoc[usedTreasure[4]][0] = random.nextInt(0,6);
+            treasureLoc[usedTreasure[4]][1] = random.nextInt(7,11);
+            for (int i = 0; i < 20; i++) {
+                if ( treasureLoc[usedTreasure[4]][0] != UsedNumberInt[i][0]
+                        || treasureLoc[usedTreasure[4]][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = treasureLoc[usedTreasure[4]];
+                UsedNumberIntArrey++;
+                break;
+            }
+        }
+
+      while (true){
+            boolean OutOfLoop = true;
+            treasureLoc[usedTreasure[5]][0] = random.nextInt(0,6);
+            treasureLoc[usedTreasure[5]][1] = random.nextInt(7,11);
+            for (int i = 0; i < 20; i++) {
+                if ( treasureLoc[usedTreasure[5]][0] != UsedNumberInt[i][0]
+                        || treasureLoc[usedTreasure[5]][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = treasureLoc[usedTreasure[5]];
+                UsedNumberIntArrey++;
+                break;
+            }
+        }
+
+      while (true){
+            boolean OutOfLoop = true;
+            treasureLoc[usedTreasure[6]][0] = random.nextInt(7,11);
+            treasureLoc[usedTreasure[6]][1] = random.nextInt(7,11);
+            for (int i = 0; i < 20; i++) {
+                if ( treasureLoc[usedTreasure[6]][0] != UsedNumberInt[i][0]
+                        || treasureLoc[usedTreasure[6]][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = treasureLoc[usedTreasure[6]];
+                UsedNumberIntArrey++;
+                break;
+            }
+        }
+
+      while (true){
+            boolean OutOfLoop = true;
+            treasureLoc[usedTreasure[7]][0] = random.nextInt(7,11);
+            treasureLoc[usedTreasure[7]][1] = random.nextInt(7,11);
+            for (int i = 0; i < 20; i++) {
+                if ( treasureLoc[usedTreasure[7]][0] != UsedNumberInt[i][0]
+                        || treasureLoc[usedTreasure[7]][1] != UsedNumberInt[i][1]) {
+                    continue;
+                } else {
+                    OutOfLoop = false;
+                    break;
+                }
+            }
+            if (OutOfLoop) {
+                UsedNumberInt[UsedNumberIntArrey] = treasureLoc[usedTreasure[7]];
+                UsedNumberIntArrey++;
+                break;
+            }
+        }
+
+
+        for (int i = 0; i < UsedNumberIntArrey; i++) {
+            System.out.println(UsedNumberInt[i][0]);
+            System.out.println(UsedNumberInt[i][1]);
+            System.out.println("UsedNumberInt[i][1]");
+
+
+        }    }
+
+
+
     private void initPanle() {
         backBtnGame = new JButton();
         backBtnGame.setBackground(Color.gray);
@@ -66,8 +432,8 @@ public class Game extends JFrame implements ActionListener{
         add(backBtnGame);
         backBtnGame.addActionListener(this);
 
-
-
+        diceRand.DiceBtn.setBounds(20,610,100,100);
+        add(diceRand.DiceBtn);
 
 
 
@@ -97,12 +463,28 @@ public class Game extends JFrame implements ActionListener{
         Graphics2D g2 = (Graphics2D) g;
         g2.drawImage(backImg,0,0,null);
         g2.drawImage(castleIcn,800,380,60,60,null);
+        for (int i = 0; i < 5; i++) {
+            g2.drawImage(marketIcn,MarketLocVar[i][1]*cellSize+leftMargin
+                    , MarketLocVar[i][0]*cellSize+topMargin
+                    , cellSize,cellSize,null
+            );
+        }
+        for (int i = 0; i < 8; i++) {
+            g2.drawImage(treasureIcn,treasureLoc[i][1]*cellSize+leftMargin
+                    , treasureLoc[i][0]*cellSize+topMargin
+                    , cellSize,cellSize,null
+            );
+        }
+
         g2.setStroke(new BasicStroke(3));
         g2.setColor(Color.lightGray);
         for (int i = 0;i<11;i++){
             for(int j = 0;j<11;j++){
 
-                g2.drawRect(rectangle[i][j].x,rectangle[i][j].y,rectangle[i][j].width,rectangle[i][j].height);
+                g2.drawRect(rectangle[i][j].x
+                        ,rectangle[i][j].y
+                        ,rectangle[i][j].width
+                        ,rectangle[i][j].height);
             }
         }
 
--- src/random_number.java
+++ src/random_number.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
-public class random_number {
+public class random_number implements ActionListener {
 
+    public JLabel DiceLbl;
+    public JButton DiceBtn;
+    private int Dice;
+    JLabel mainDicelbl;
     Random random;
     Toolkit toolkit = Toolkit.getDefaultToolkit();
 
@@ -15,42 +21,53 @@ public class random_number {
     public ImageIcon Dice6 = new ImageIcon("Dice6.png");
 
 
-    JLabel mainDicelbl;
 
 
-    private int Dice;
-    random_number(){
 
+
+    public random_number(){
+
+        DiceBtn = new JButton(Dice6);
+        DiceBtn.addActionListener(this);
+
+         Dice = getDice();
+    }
+
+    private void makeDiceNumber() {
         random = new Random();
-        Dice = 6;
-        mainDicelbl = new JLabel(Dice6);
-       // Dice = random.nextInt(0,6);
+        Dice = random.nextInt(0,6);
         switch (Dice){
             case 1:
-            mainDicelbl.setIcon((Icon) Dice1);
-            break;
+                DiceBtn.setIcon((Icon) Dice1);
+                break;
             case 2:
-                mainDicelbl.setIcon((Icon) Dice2);
+                DiceBtn.setIcon((Icon) Dice2);
                 break;
             case 3:
-                mainDicelbl.setIcon((Icon) Dice3);
+                DiceBtn.setIcon((Icon) Dice3);
                 break;
             case 4:
-                mainDicelbl.setIcon((Icon) Dice4);
+                DiceBtn.setIcon((Icon) Dice4);
                 break;
             case 5:
-                mainDicelbl.setIcon((Icon) Dice5);
+                DiceBtn.setIcon((Icon) Dice5);
                 break;
             case 6:
-                mainDicelbl.setIcon((Icon) Dice6);
+                DiceBtn.setIcon((Icon) Dice6);
                 break;
 
         }
-        getDice();
     }
 
     public int getDice() {
 
         return Dice;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource().equals(DiceBtn)){
+            makeDiceNumber();
+        }
     }
 }
